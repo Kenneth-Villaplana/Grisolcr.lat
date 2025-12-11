28097fa558b5fe43f9e3ce524e65f784deee0172
Juliana301 <julianagonzalez3015@gmail.com>
2025-12-11 01:08:35 -0600

    cambios

--- View/layout.php
+++ View/layout.php
@@ -30,7 +30,7 @@ function MostrarMenu() {
                 Citas
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownCitas">
-                <li><a class="dropdown-item" href="/OptiGestion/view/appointmentForm.php">Agendar Cita</a></li>
+                <li><a class="dropdown-item" href="/OptiGestion/view/agendarCita.php">Agendar Cita</a></li>
                 <li><a class="dropdown-item" href="/OptiGestion/view/editarcita.php">Mis Citas</a></li>
                 <li><a class="dropdown-item" href="/OptiGestion/view/misRecetas.php">Historial Médico</a></li>
             </ul>
@@ -144,7 +144,7 @@ function IncluirCSS() {
     
     <link rel="stylesheet" href="/OptiGestion/assets/vendor/glightbox/css/glightbox.min.css">
     <link rel="stylesheet" href="/OptiGestion/assets/vendor/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="/OptiGestion/assets/css/styles.css?v=8.9">
+    <link rel="stylesheet" href="/OptiGestion/assets/css/styles.css?v=10.2">
     <link rel="icon" type="image/x-icon" href="/OptiGestion/assets/favicon.ico">
     ';
 }
