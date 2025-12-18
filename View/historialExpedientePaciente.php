baa9fbf8eaa6e9e9660493d7a450d50834d99b4d
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2025-12-18 17:17:29 -0600

    Recetas e historial paciente

--- View/historialExpedientePaciente.php
+++ View/historialExpedientePaciente.php
@@ -110,7 +110,7 @@ if (empty($historial))
         </div>
     </div>
 
-<script src="Grisolcr.lat/assets/js/receta.js"></script>
+<script src="../assets/js/receta.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
