6365d769e95bfa7130defbd6a5d6c9445a0910c3
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-02-14 19:50:59 -0600

    Update baseDatos.php

--- Model/baseDatos.php
+++ Model/baseDatos.php
@@ -1,15 +1,28 @@
 <?php
 
 // Abre una conexion a la base de datos
-function AbrirBD()
+
+function AbrirBD(): mysqli
 {
-    $conn = mysqli_connect(
-    "grisolcr-server.mysql.database.azure.com",
-    "hvehxcvtoq",
-    "Y$4JjJZJIxHieE2G",
-    "optigestion",
-    3306
-);
+    $host = "grisolcr-server.mysql.database.azure.com";
+    $user = "hvehxcvtoq";
+    $pass = "Y$4JjJZJIxHieE2G";
+    $db   = "optigestion";
+    $port = 3306;
+
+    $conn = mysqli_init();
+
+    mysqli_real_connect(
+        $conn,
+        $host,
+        $user,
+        $pass,
+        $db,
+        $port,
+        NULL,
+        MYSQLI_CLIENT_SSL
+    );
+
 
     if (!$conn) {
         die("Error de conexión: " . mysqli_connect_error());
