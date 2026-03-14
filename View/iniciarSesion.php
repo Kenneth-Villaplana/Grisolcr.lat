c47adc58a3cc0e74d94fccfd5ef9bcce9fe419ae
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-14 01:33:07 -0600

    mas cambios

--- View/iniciarSesion.php
+++ View/iniciarSesion.php
@@ -1,4 +1,7 @@
 <?php 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
 if(session_status() == PHP_SESSION_NONE) { session_start(); }
 include_once 'layout.php';
 include_once __DIR__ . '/../Controller/loginController.php';
