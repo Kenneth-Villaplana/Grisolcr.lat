01593d9492e6a9486108ce39d06d69f350155c31
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-14 00:48:47 -0600

    Update registrarPaciente.php

--- View/registrarPaciente.php
+++ View/registrarPaciente.php
@@ -11,7 +11,7 @@ include_once __DIR__ . '/Controller/loginController.php';
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Óptica Grisol</title>
-     <script src="assets/js/registro.js"></script>
+    
     <?php IncluirCSS();?>
 </head>
 <body>
