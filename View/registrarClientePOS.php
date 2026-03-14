a0467beab87fa082acf44ee8f3d48250dccd7112
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-13 23:59:26 -0600

    rutas

--- View/registrarClientePOS.php
+++ View/registrarClientePOS.php
@@ -1,6 +1,6 @@
 <?php 
 include_once 'layout.php';
-include_once __DIR__ . '/../Controller/loginController.php';
+include_once __DIR__ . '/Controller/loginController.php';
 
 $cedulaPrefill = $_GET['cedula'] ?? '';
 ?>
