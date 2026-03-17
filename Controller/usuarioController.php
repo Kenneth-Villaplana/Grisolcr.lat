ad1fb085ca3d3092c1d52ffcfb8db5d5f884a73d
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-17 00:25:17 -0600

    Update usuarioController.php

--- Controller/usuarioController.php
+++ Controller/usuarioController.php
@@ -1,10 +1,11 @@
 <?php
-include_once __DIR__ . '/../Model/LoginModel.php';
-include_once __DIR__ . '/../Model/UsuarioModel.php';
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+include_once __DIR__ . '/../Model/LoginModel.php';
+include_once __DIR__ . '/../Model/UsuarioModel.php';
+
+
 
 /*
 |--------------------------------------------------------------------------
