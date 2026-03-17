d2bde6537c235dfbaa77d7c502436e5fd553e193
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-17 00:59:41 -0600

    arreglos

--- View/editarPerfil.php
+++ View/editarPerfil.php
@@ -1,10 +1,7 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-include('layout.php');
+session_start();
 require_once __DIR__ . '/../Controller/usuarioController.php';
+include('layout.php');
 ?>
 
 <!DOCTYPE html>
