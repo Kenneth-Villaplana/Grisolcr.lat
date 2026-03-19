c585389abbc9e703032e17a042fcacdadd3c19e6
Juliana301 <julianagonzalez3015@gmail.com>
2026-03-18 21:44:45 -0600

    Update historialExpedientePaciente.php

--- View/historialExpedientePaciente.php
+++ View/historialExpedientePaciente.php
@@ -111,8 +111,8 @@ if (empty($historial))
         </div>
     </div>
 
-<script src="/assets/js/receta.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/receta.js?v=5"></script>
 </body>
 </html>
