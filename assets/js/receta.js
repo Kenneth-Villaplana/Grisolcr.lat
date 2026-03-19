945d715a1e2a2bbd72e045f1fc1892ac9b2bc900
Juliana301 <julianagonzalez3015@gmail.com>
2026-03-18 21:16:23 -0600

    cambios

--- assets/js/receta.js
+++ assets/js/receta.js
@@ -1,9 +1,25 @@
-function cargarReceta(idExpediente) {
-    const url = "/View/recetaParaDoctor.php?IdExpediente=" + idExpediente;
+function imprimirReceta() {
+    const contenido = document.getElementById("contenedorReceta").innerHTML;
 
-    const ventana = window.open(url, "_blank");
+    const ventana = window.open('', '', 'width=800,height=600');
+
+    ventana.document.write(`
+        <html>
+        <head>
+            <title>Imprimir Receta</title>
+            <link rel="stylesheet" href="/assets/css/styles.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+            ${contenido}
+        </body>
+        </html>
+    `);
+
+    ventana.document.close();
 
     ventana.onload = function () {
         ventana.print();
+        ventana.close();
     };
 }
