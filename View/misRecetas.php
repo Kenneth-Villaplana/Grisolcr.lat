fda1ed8cc9cb5643e5937272b1ea0378282b10ef
Juliana301 <julianagonzalez3015@gmail.com>
2026-03-18 20:36:15 -0600

    Update misRecetas.php

--- View/misRecetas.php
+++ View/misRecetas.php
@@ -1,6 +1,6 @@
 <?php
 include('layout.php');
-require_once __DIR__ . '../Controller/recetaController.php';
+require_once __DIR__ . '/../Controller/recetaController.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
