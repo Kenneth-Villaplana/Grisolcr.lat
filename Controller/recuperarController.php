02de78a60decbf33581c40425d5a87d1bb8df97d
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-25 18:41:35 -0600

    Update recuperarController.php

--- Controller/recuperarController.php
+++ Controller/recuperarController.php
@@ -1,7 +1,9 @@
+<?php
+// MOSTRAR ERRORES (DEBUG)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-<?php
+
 session_start();
 include_once __DIR__ . '/../Model/recuperarModel.php';
 
