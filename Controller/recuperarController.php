b20ac37804a593a24706c4c8ea2e9962c9f8256d
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-25 19:26:17 -0600

    Update recuperarController.php

--- Controller/recuperarController.php
+++ Controller/recuperarController.php
@@ -9,7 +9,7 @@ include_once __DIR__ . '/../Model/recuperarModel.php';
 
 function cargarConfigMail(): array
 {
-    $ruta = __DIR__ . '/../config/mail.example.json';
+    $ruta = __DIR__ . '/../config/mail.json';
 
     if (!file_exists($ruta)) {
         return [];
