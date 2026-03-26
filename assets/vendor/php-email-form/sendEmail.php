5e3238934b6ce11c2d918505ee61fd647834f8fd
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-25 19:29:51 -0600

    Update sendEmail.php

--- assets/vendor/php-email-form/sendEmail.php
+++ assets/vendor/php-email-form/sendEmail.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 function cargarConfigMailJson(): array
 {
-    $ruta = __DIR__ . '/../../../config/mail.json';
+    $ruta = '/var/www/html/config/mail.json';
 
     if (!file_exists($ruta)) {
         return [];
