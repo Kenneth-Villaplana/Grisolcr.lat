aa8b741f617e0bb670b816df298d4c25d4457306
Juliana301 <julianagonzalez3015@gmail.com>
2026-03-26 20:53:30 -0600

    css

--- View/registrarPaciente.php
+++ View/registrarPaciente.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/../Controller/loginController.php';
 <html lang="es">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Óptica Grisol</title>
@@ -17,7 +17,7 @@ include_once __DIR__ . '/../Controller/loginController.php';
 <body>
     <?php MostrarMenu();?>
 
-    <section class="editar-section d-flex align-items-center justify-content-center py-5 my-5">
+    <section class="editar-section d-flex align-items-center justify-content-center py-4">
 
     <div class="container">
 
