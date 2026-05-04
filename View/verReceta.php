75a53118186a57a53e8d3850f1ac09d423a81162
Li-desing <lleiva10893@ufide.ac.cr>
2026-05-03 21:32:49 -0600

    update ver receta

--- View/verReceta.php
+++ View/verReceta.php
@@ -105,6 +105,7 @@ if (!$receta) {
                         <th>CIL</th>
                         <th>EJE</th>
                         <th>PRISMA</th>
+                        <th>ADD</th>
                         
                     </tr>
                 </thead>
@@ -115,6 +116,7 @@ if (!$receta) {
                         <td><?= $receta['Cilindro_OD'] ?></td>
                         <td><?= $receta['Eje_OD'] ?></td>
                         <td><?= $receta['Prisma_OD'] ?></td>
+                        <td><?= $receta['ADD_OD'] ?></td>
                         
                     </tr>
 
@@ -124,6 +126,7 @@ if (!$receta) {
                         <td><?= $receta['Cilindro_OI'] ?></td>
                         <td><?= $receta['Eje_OI'] ?></td>
                         <td><?= $receta['Prisma_OI'] ?></td>
+                        <td><?= $receta['ADD_OI'] ?></td>
                         
                     </tr>
                 </tbody>
